172e8865b7da1ac19d9a1668a042874e73e3326c
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-06 13:16:09 -0800

    problem1 done

--- problem1.py
+++ problem1.py
@@ -11,7 +11,7 @@
 
 """ Sample output:
 Enter in the coefficients for a quadratic equation in the format:
-  ax^2 + bx + c = 0
+  ax^2 + bx + c = 0 
 a:3
 b:d
 c:1
@@ -31,7 +31,47 @@ The roots are -4.0 and -4.0
 """
 import os
 os.system('cls')
+import math
 
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+def solve_quadratic(a, b, c):
+  d = b**2 - 4*a*c
+  if d < 0:
+    print("There are no real roots to the equation")
+  elif d == 0:
+    x = (-b + math.sqrt(d)) / (2 * a)
+    x = round(x, 2)
+    print(f"There is one solution: {x}")
+  else:
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    x1 = round(x1, 2)
+    x2 = round(x2, 2)
+    print(f"There are two solutions: {x1} and {x2}")
+
+while True:
+  while True:
+    a = input("Enter the coefficient a: ")
+    b = input("Enter the coefficient b: ")
+    c = input("Enter the coefficient c: ")
+    if is_number(a) and is_number(b) and is_number(c):
+      a = float(a)
+      b = float(b)
+      c = float(c)
+      break
+    else:
+      print("Those are not valid values for a, b, or c")
+  solve_quadratic(a, b, c)
+  answer = input("Do you want to try another equation? (y/n) ")
+  if answer.lower() == "n":
+    print("Goodbye!")
+    break
 
 print("Enter in the coefficients for a quadratic equation in the format:")
 print("  ax^2 + bx + c = 0")
